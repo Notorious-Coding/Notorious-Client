407df7582ea7fa1c023ce9ec277fc91d4358c31d
Brice SCHUMACHER <briceschumacher21@gmail.com>
2025-10-26 11:58:03 +0100

    chore : (RequestBuilder) Adding support for versioned api

---
+++ NotoriousClient/Builder/VersionedEndpoint.cs
@@ -0,0 +1,11 @@
+﻿namespace NotoriousClient.Builder
+{
+    public class VersionedEndpoint : Endpoint
+    {
+        public string Version { get; set; }
+        public VersionedEndpoint(string route, Method method, string version) : base(route, method)
+        {
+            Version = version;
+        }
+    }
+}
