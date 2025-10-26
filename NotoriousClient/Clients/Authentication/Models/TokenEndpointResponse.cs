b869df5bcff09c68a8370d65f1b12d4f5ab094a4
Brice SCHUMACHER <briceschumacher21@gmail.com>
2025-10-26 21:45:44 +0100

    feat : (BaseClient) Added support for Client Credentials flow

---
+++ NotoriousClient/Clients/Authentication/Models/TokenEndpointResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotoriousClient.Clients.Authentication.Models
+{
+    public class TokenEndpointResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = string.Empty;
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_expires_in")]
+        public int RefreshExpiresIn { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = string.Empty;
+
+        [JsonPropertyName("not-before-policy")]
+        public int NotBeforePolicy { get; set; }
+
+        [JsonPropertyName("scope")]
+        public string Scope { get; set; } = string.Empty;
+    }
+
+}
